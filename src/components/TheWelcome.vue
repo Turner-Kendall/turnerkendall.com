ae9077b5eba35be11892841f5ed93dbc3654125d
turner-kendall <tk@turnerkendall.com>
2024-08-15 19:42:52 -0400

    poist deploy

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -10,30 +10,26 @@ import ExperienceIcon from './icons/IconExperience.vue'
   <WelcomeItem>
     <template #icon> <NinjaIcon /> </template>
     <template #heading>Who is (James) Turner Kendall?</template>
-
     You might know me as <a href="https://tripkendall.com" target="_blank" rel="noopener">
-      Trip Kendall</a> <a href="https://x.com/tr1pwyr">@tr1pwyr</a> James, or even 
-    <a href="http://web.archive.org/web/20120212150556/http://imjtk.com/">JTk</a> - hey I've been around a while.  Right now
-    I am writing Rust and it is an entirely new experience. 
+    Trip Kendall</a> <a href="https://x.com/tr1pwyr">@tr1pwyr</a> James, or even 
+    <a href="http://web.archive.org/web/20120212150556/http://imjtk.com/">JTk</a> - hey I've been around a while.  
+    Right now I am writing Rust and it is an entirely new experience. 
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon> <PreviousIcon /> </template>
     <template #heading>Previously</template>
-
-   <ul>
-    <li>AtMorph — Senior Developer</li>
-    <li>D2 Interactive — COO</li>
-    <li>eWebCorp - CTO </li>
-    <li>Kaboodle Inc. - Lead Developer</li>
-   </ul>
-
+    <ul>
+      <li>AtMorph — Senior Developer</li>
+      <li>D2 Interactive — COO</li>
+      <li>eWebCorp - CTO </li>
+      <li>Kaboodle Inc. - Lead Developer</li>
+    </ul>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon> <IconSkills /> </template>
     <template #heading>Skills</template>
-
     I write code and manage dev stacks and servers. 
     I like VSCode, Git, Rust, and Jet Brains Mono. I also code in Python, Javascript, and Go. 
     I have experience with multiple databases and a plethora of frameworks.
@@ -43,9 +39,8 @@ import ExperienceIcon from './icons/IconExperience.vue'
   <WelcomeItem>
     <template #icon> <ExperienceIcon /> </template>
     <template #heading>Experience</template>
-
     I Co-founded what became the biggest web dev company in the state, created Nikki Haley's website when she ran for governor, and delivered literally dozens
-    of custom solutions as an indy dev... <a href="/about">-></a>
+    of custom solutions as an indy dev <a href="/about">-></a>
   </WelcomeItem>
 
 </template>
